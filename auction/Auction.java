0ca98e7dbf04babd5d544834aa7cc456b8f05ca2
carot15 <tridung2727@gmail.com>
2026-05-03 21:31:15 +0700

    cài đặt thêm class auction

--- auction/Auction.java
+++ auction/Auction.java
@@ -21,5 +21,41 @@ public class Auction {
     private Bidder highestBidder;
     private AuctionStatus status;
     private List<BidTransaction> bidHistory;
+
+    public Auction(Item item, LocalDateTime startTime, LocalDateTime endTime) {
+        this.item = item;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.currentHighestBid = item.getStartingPrice(); 
+        this.status = AuctionStatus.OPEN;
+        this.bidHistory = new ArrayList<>();
+    }
     
+    // xu li luog an toan
+    public synchronized boolean placeBid(Bidder bidder, double amount) 
+            throws InvalidBidException, AuctionClosedException {
+        
+        LocalDateTime now = LocalDateTime.now();
+
+        if (this.status != AuctionStatus.RUNNING) {
+            throw new AuctionClosedException("Phiên đấu giá không trong trạng thái hoạt động!");
+        }
+        if (now.isAfter(this.endTime)) {
+            closeAuction();
+            throw new AuctionClosedException("Phiên đấu giá đã kết thúc!");
+        }
+        if (amount <= this.currentHighestBid) {
+            throw new InvalidBidException("Giá đặt (" + amount + ") phải lớn hơn giá hiện tại (" + this.currentHighestBid + ")");
+        }
+
+        // Cap nhat gia ...
+        return true;
+    }
+    public synchronized void closeAuction() {
+        if (this.status != AuctionStatus.FINISHED) {
+            this.status = AuctionStatus.FINISHED;
+            System.out.println("Đóng phiên.");
+        }
+    }
+
 }
