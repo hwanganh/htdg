863c6838a6618e602dffbac3f11d80af67b2226d
25023464-commits <25023464@vnu.edu.vn>
2026-05-04 15:26:48 +0700

    add description

--- entity/Item.java
+++ entity/Item.java
@@ -4,11 +4,12 @@ public abstract class Item extends Entity{
     protected String id;
     protected String name;
     protected double startingPrice;
-
-    public Item(String id, String name, double startingPrice) {
+    protected String description;
+    public Item(String id, String name, double startingPrice, String description) {
         this.id = id;
         this.name = name;
         this.startingPrice = startingPrice;
+        this.description = description;
     }
 
     public abstract String getDetails(); 
@@ -16,8 +17,10 @@ public abstract class Item extends Entity{
     public String getID() { return this.id; }
     public String getName() { return this.name; }
     public double getStartingPrice() { return this.startingPrice; }
+    public String getDescription(){return description;}
 
     void setID(String id) { this.id = id; }
     void setName(String name) { this.name = name; }
     void setStartingPrice(double startingPrice) { this.startingPrice = startingPrice; }
+    void setDescription(String description) { this.description = description;}
 }
