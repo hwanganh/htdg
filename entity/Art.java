21eff9e6a21c47739e3f7072618fe00c3590d570
25023464-commits <25023464@vnu.edu.vn>
2026-05-05 15:11:03 +0700

    Change Art constructor parameter type from String to int

--- entity/Art.java
+++ entity/Art.java
@@ -3,7 +3,7 @@ package entity;
 public class Art extends Item {
     private String artist;
 
-    public Art(String id, String name, double startingPrice, String artist) {
+    public Art(int id, String name, double startingPrice, String artist) {
         super(id, name, startingPrice);
         this.artist = artist;
     }
