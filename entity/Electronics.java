a7dd2eaa702adebbe9c41068beea228602d57622
25023464-commits <25023464@vnu.edu.vn>
2026-05-05 15:11:28 +0700

    Change Electronics constructor parameter type for id

--- entity/Electronics.java
+++ entity/Electronics.java
@@ -3,7 +3,7 @@ package entity;
 public class Electronics extends Item {
     private int warrantyMonths;
 
-    public Electronics(String id, String name, double startingPrice, int warrantyMonths) {
+    public Electronics(int id, String name, double startingPrice, int warrantyMonths) {
         super(id, name, startingPrice);
         this.warrantyMonths = warrantyMonths;
     }
