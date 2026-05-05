d9dd40fbda8e2ed2cbeaf1eb0af0190ce02955e6
25023464-commits <25023464@vnu.edu.vn>
2026-05-05 15:12:46 +0700

    Change id type from String to int in Item class

--- entity/Item.java
+++ entity/Item.java
@@ -1,11 +1,11 @@
 package entity;
 
 public abstract class Item extends Entity{
-    protected String id;
+    protected int id;
     protected String name;
     protected double startingPrice;
     protected String description;
-    public Item(String id, String name, double startingPrice, String description) {
+    public Item(int id, String name, double startingPrice, String description) {
         this.id = id;
         this.name = name;
         this.startingPrice = startingPrice;
@@ -14,12 +14,11 @@ public abstract class Item extends Entity{
 
     public abstract String getDetails(); 
 
-    public String getID() { return this.id; }
+    public int getID() { return this.id; }
     public String getName() { return this.name; }
     public double getStartingPrice() { return this.startingPrice; }
     public String getDescription(){return description;}
-
-    void setID(String id) { this.id = id; }
+    
     void setName(String name) { this.name = name; }
     void setStartingPrice(double startingPrice) { this.startingPrice = startingPrice; }
     void setDescription(String description) { this.description = description;}
